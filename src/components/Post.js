98cffe67168fc961c5345c4a835fc64ff6c0e9f0
Hao Cui <raggedbear45@gmail.com>
2021-08-22 00:05:11 -0400

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -24,8 +24,8 @@ const Post = () => {
     return (
         <main className="bg-green-100 min-h-screen p-12">
             <section className="container mx-auto">
-                <h1 className="text-5xl flex justify-center cursive">title</h1>
-                <h2 className="text-lg text-gray-600 flex justify-center mb-12">subtitle</h2>
+                <h1 className="text-5xl flex justify-center cursive">My Blog Page</h1>
+                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my Blog page</h2>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {postData && postData.map((post, index) => (
                     <article>
